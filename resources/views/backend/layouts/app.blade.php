818f0d107889dd0ba2a70a88dcfb6ff86fceffd9
Patrick Her <renfu.her@gmail.com>
2023-04-08 12:00:40 +0800

    update favicon.png

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
+    <link rel="icon" type="image/x-icon" href="/favicon.png">
     <title>Admin</title>
     <link rel="stylesheet" href="{{ asset('admin/css/bootstrap.min.css') }}" >
     <link rel="stylesheet" href="{{ asset('admin/css/all.min.css') }}">
