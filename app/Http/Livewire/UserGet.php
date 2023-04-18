982cc1b99c16f359ebf779210dca4ee9ff5dd9d4
Patrick Her <renfu.her@gmail.com>
2023-04-18 17:12:58 +0800

    update livewire

---
+++ app/Http/Livewire/UserGet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UserGet extends Component
+{
+    public function render()
+    {
+        return view('livewire.user-get');
+    }
+}
